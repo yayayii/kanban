4892f61a5f8c1edef642002a20e60ed7b11dc6bf
yayayi <evgegnii-homichk@yandex.ru>
2023-06-26 03:23:26 +0300

    feat: added task entity

---
+++ src/main/java/org/example/kanban/enum_/TaskStatus.java
@@ -0,0 +1,17 @@
+package org.example.kanban.enum_;
+
+public enum TaskStatus {
+    NEW("New"),
+    IN_PROGRESS("In progress"),
+    DONE("Done");
+
+    private final String statusName;
+
+    TaskStatus(String statusName) {
+        this.statusName = statusName;
+    }
+
+    public String getStatusName() {
+        return statusName;
+    }
+}
