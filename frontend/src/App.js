37b382ff63daa13926c0dee3c15edfe88b1b9e3d
yayayi <evgegnii-homichk@yandex.ru>
2023-06-26 04:01:02 +0300

    feat: created "create" page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,16 +3,18 @@ import './App.css';
 import {BrowserRouter as Router, Route, Routes} from 'react-router-dom';
 import Home from './Home';
 import AppNavbar from "./AppNavbar";
+import TaskCreate from "./TaskCreate";
 
 const App = () => {
-  return (
-      <Router>
+    return (
+        <Router>
         <AppNavbar/>
-        <Routes>
-          <Route path='/' exact={true} element={<Home/>}/>
-        </Routes>
-      </Router>
-  )
+            <Routes>
+                <Route path='/' exact={true} element={<Home/>}/>
+                <Route path='/create' exact={true} element={<TaskCreate/>}/>
+            </Routes>
+        </Router>
+    )
 }
 
 export default App;
